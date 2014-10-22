49bed8deadb4ff17f8a7fa0d3a1dec46c36036c3
Stephen Berquet <stephen.berquet@gmail.com>
2014-10-22 23:29:46 +0200

    update

--- cryptopp_test.cpp
+++ cryptopp_test.cpp
@@ -1,9 +1,18 @@
-#include <php.h>
+#define CRYPTOPP_ENABLE_NAMESPACE_WEAK 1
+
+#include <iostream>
+#include <string>
+#include <cstdio>
+
 #include "php_cryptopp_test.h"
+
 #include "aes.h"
+#include "md5.h"
+#include "sha.h"
 
 zend_function_entry cryptopp_test_functions[] = {
-    PHP_FE(cryptopp_test, NULL)
+    PHP_FE(cryptopp_md5, NULL)
+    PHP_FE(cryptopp_sha1, NULL)
     {NULL, NULL, NULL}
 };
 
@@ -20,9 +29,51 @@ zend_module_entry cryptopp_test_module_entry = {
     STANDARD_MODULE_PROPERTIES
 };
 
+#ifdef COMPILE_DL_CRYPTOPP_TEST
+extern "C" {
 ZEND_GET_MODULE(cryptopp_test)
+}
+#endif
 
-PHP_FUNCTION(cryptopp_test) {
-    php_printf("aes block size : %ld\n", CryptoPP::AES::BLOCKSIZE);
+void outputByteArray(byte *byteArray, int size) {
+    for (int i = 0; i < size; i++) {
+        php_printf("%ld:", byteArray[i]);
+    }
+
+    php_printf("\n");
+}
+
+PHP_FUNCTION(cryptopp_md5) {
+    char *msg   = NULL;
+    int msgSize = 0;
+
+    if (FAILURE == zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &msg, &msgSize)) {
+        return;
+    }
+
+    php_printf("md5 digest size : %ld\n", CryptoPP::Weak::MD5::DIGESTSIZE);
+
+    CryptoPP::Weak::MD5 hash;
+    byte digest[CryptoPP::Weak::MD5::DIGESTSIZE];
+    hash.CalculateDigest(digest, (byte*) msg, msgSize);
+
+    outputByteArray(digest, sizeof(digest));
+}
+
+PHP_FUNCTION(cryptopp_sha1) {
+    char *msg   = NULL;
+    int msgSize = 0;
+
+    if (FAILURE == zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &msg, &msgSize)) {
+        return;
+    }
+
+    php_printf("sha1 digest size : %ld\n", CryptoPP::SHA::DIGESTSIZE);
+
+    CryptoPP::SHA hash;
+    byte digest[CryptoPP::SHA::DIGESTSIZE];
+    hash.CalculateDigest(digest, (byte*) msg, msgSize);
+
+    outputByteArray(digest, sizeof(digest));
 }
 
--- php_cryptopp_test.h
+++ php_cryptopp_test.h
@@ -1,5 +1,22 @@
+#ifndef PHP_CRYPTOPP_TEST
+#define PHP_CRYPTOPP_TEST
+
 #define PHP_CRYPTOPP_TEST_EXTNAME "cryptopp_test"
 #define PHP_CRYPTOPP_TEST_VERSION "0.1"
 
-PHP_FUNCTION(cryptopp_test);
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+extern "C" {
+#include "php.h"
+}
+
+extern zend_module_entry cryptopp_test_module_entry;
+#define phpext_cryptopp_test_ptr &cryptopp_test_module_entry;
+
+PHP_FUNCTION(cryptopp_md5);
+PHP_FUNCTION(cryptopp_sha1);
+
+#endif
 
