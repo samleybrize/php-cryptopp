3a805233cce71b97f92a78b7b74b62faf45f2f9d
Stephen Berquet <stephen.berquet@gmail.com>
2014-10-27 23:19:10 +0100

    factorise hash classes definition

--- src/hash/php_hash_interface.h
+++ src/hash/php_hash_interface.h
@@ -5,6 +5,52 @@ extern "C" {
 #include "php.h"
 }
 
+// TODO type->properties_info ?????
+#define CRYPTOPP_HASH_INIT_CLASS(classname, nativeClassname, classEntryPtrName, classMethodsVarName) \
+    zend_object_handlers classname ## _object_handlers;                     \
+    struct classname ## Container {                                         \
+        zend_object std;                                                    \
+        nativeClassname *hash;                                              \
+    };                                                                      \
+    void classname ## _free_storage(void *object TSRMLS_DC) {               \
+        classname ## Container *obj = (classname ## Container *) object;    \
+        delete obj->hash;                                                   \
+        zend_hash_destroy(obj->std.properties);                             \
+        FREE_HASHTABLE(obj->std.properties);                                \
+        efree(obj);                                                         \
+    }                                                                       \
+    zend_object_value classname ## _create_handler(zend_class_entry *type TSRMLS_DC) { \
+        zval *tmp;                                                          \
+        zend_object_value retval;                                           \
+                                                                            \
+        classname ## Container *obj = (classname ## Container *) emalloc(sizeof(classname ## Container)); \
+        memset(obj, 0, sizeof(classname ## Container));                     \
+        obj->std.ce = type;                                                 \
+                                                                            \
+        ALLOC_HASHTABLE(obj->std.properties);                               \
+        zend_hash_init(obj->std.properties, 0, NULL, ZVAL_PTR_DTOR, 0);     \
+        zend_hash_copy(obj->std.properties, &type->properties_info, (copy_ctor_func_t)zval_add_ref, (void *)&tmp, sizeof(zval *)); \
+                                                                            \
+        retval.handle   = zend_objects_store_put(obj, NULL, HashSha1_free_storage, NULL TSRMLS_CC); \
+        retval.handlers = &classname ## _object_handlers;                   \
+                                                                            \
+        return retval;                                                      \
+    }                                                                       \
+    void init_class_ ## classname(TSRMLS_D) {                               \
+        zend_class_entry ce;                                                \
+        INIT_CLASS_ENTRY(ce, #classname, classMethodsVarName);              \
+        classEntryPtrName = zend_register_internal_class(&ce TSRMLS_CC);    \
+                                                                            \
+        classEntryPtrName->create_object = classname ## _create_handler;    \
+        memcpy(&classname ## _object_handlers, zend_get_std_object_handlers(), sizeof(zend_object_handlers)); \
+        classname ## _object_handlers.clone_obj = NULL;                     \
+                                                                            \
+        zend_class_implements(classEntryPtrName TSRMLS_CC, 1, cryptopp_test_ce_hash_interface); \
+    }
+
+#define CRYPTOPP_HASH_GET_NATIVE_PTR(classname) ((classname ## Container *)zend_object_store_get_object(getThis() TSRMLS_CC))->hash
+#define CRYPTOPP_HASH_SET_NATIVE_PTR(classname, nativeHashPtr) ((classname ## Container *)zend_object_store_get_object(getThis() TSRMLS_CC))->hash = nativeHashPtr;
+
 extern zend_class_entry *cryptopp_test_ce_hash_interface;
 
 void init_interface_HashInterface(TSRMLS_D);
--- src/hash/php_sha1.cpp
+++ src/hash/php_sha1.cpp
@@ -4,44 +4,6 @@
 #include <sha.h>
 #include <map>
 
-zend_object_handlers HashSha1_object_handlers;
-
-struct HashSha1Container {
-    zend_object std;
-    CryptoPP::SHA1 *hash;
-};
-
-void HashSha1_free_storage(void *object TSRMLS_DC)
-{
-    HashSha1Container *obj = (HashSha1Container *) object;
-    delete obj->hash;
-
-    zend_hash_destroy(obj->std.properties);
-    FREE_HASHTABLE(obj->std.properties);
-
-    efree(obj);
-}
-
-zend_object_value HashSha1_create_handler(zend_class_entry *type TSRMLS_DC)
-{
-    zval *tmp;
-    zend_object_value retval;
-
-    HashSha1Container *obj = (HashSha1Container *) emalloc(sizeof(HashSha1Container));
-    memset(obj, 0, sizeof(HashSha1Container));
-    obj->std.ce = type;
-
-    ALLOC_HASHTABLE(obj->std.properties);
-    zend_hash_init(obj->std.properties, 0, NULL, ZVAL_PTR_DTOR, 0);
-    zend_hash_copy(obj->std.properties, &type->properties_info, (copy_ctor_func_t)zval_add_ref, (void *)&tmp, sizeof(zval *));
-    // TODO type->properties_info ?????
-
-    retval.handle   = zend_objects_store_put(obj, NULL, HashSha1_free_storage, NULL TSRMLS_CC);
-    retval.handlers = &HashSha1_object_handlers;
-
-    return retval;
-}
-
 /*
  * PHP class déclaration
  */
@@ -53,26 +15,15 @@ static zend_function_entry hash_sha1_methods[] = {
     PHP_FE_END
 };
 
-void init_class_HashSha1(TSRMLS_D) {
-    zend_class_entry ce;
-    INIT_CLASS_ENTRY(ce, "HashSha1", hash_sha1_methods);
-    cryptopp_test_ce_hash_sha1                  = zend_register_internal_class(&ce TSRMLS_CC);
-
-    cryptopp_test_ce_hash_sha1->create_object   = HashSha1_create_handler;
-    memcpy(&HashSha1_object_handlers, zend_get_std_object_handlers(), sizeof(zend_object_handlers));
-    HashSha1_object_handlers.clone_obj          = NULL;
-
-    zend_class_implements(cryptopp_test_ce_hash_sha1 TSRMLS_CC, 1, cryptopp_test_ce_hash_interface);
-}
+CRYPTOPP_HASH_INIT_CLASS(HashSha1, CryptoPP::SHA1, cryptopp_test_ce_hash_sha1, hash_sha1_methods)
 
 /*
  * PHP methods definitions
  */
 PHP_METHOD(HashSha1, __construct) {
     CryptoPP::SHA1 *hash;
-    hash                    = new CryptoPP::SHA1();
-    HashSha1Container *obj  = (HashSha1Container *)zend_object_store_get_object(getThis() TSRMLS_CC);
-    obj->hash               = hash;
+    hash = new CryptoPP::SHA1();
+    CRYPTOPP_HASH_SET_NATIVE_PTR(HashSha1, hash)
 }
 
 PHP_METHOD(HashSha1, hash) {
@@ -84,8 +35,7 @@ PHP_METHOD(HashSha1, hash) {
     }
 
     CryptoPP::SHA1 *hash;
-    HashSha1Container *obj  = (HashSha1Container *)zend_object_store_get_object(getThis() TSRMLS_CC);
-    hash                    = obj->hash;
+    hash = CRYPTOPP_HASH_GET_NATIVE_PTR(HashSha1);
 
     byte digest[CryptoPP::SHA1::DIGESTSIZE];
     hash->CalculateDigest(digest, (byte*) msg, msgSize);
