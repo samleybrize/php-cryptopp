30b1801cb4a9fac368288c3156e78d8e94dd63f5
Stephen Berquet <stephen.berquet@gmail.com>
2015-01-02 22:42:14 +0100

    added AuthenticatedSymmetricCipherGeneric php stub

---
+++ php-stubs/Symmetric/Cipher/Authenticated/AuthenticatedSymmetricCipherGeneric.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Cryptopp;
+
+class AuthenticatedSymmetricCipherGeneric extends AuthenticatedSymmetricCipherAbstract
+{
+    /**
+     * Constructor
+     * @param \Cryptopp\SymmetricTransformationInterface $cipher symmetric cipher to use. Instances of \Cryptopp\AuthenticatedSymmetricCipherInterface are not allowed.
+     * @param \Cryptopp\MacInterface $mac message authentication code algorithm to use
+     * @throws \Cryptopp\CryptoppException
+     */
+    public function __construct(SymmetricTransformationInterface $cipher, MacInterface $mac) {}
+
+    /**
+     * Returns the symmetric cipher used
+     * @return \Cryptopp\SymmetricTransformationInterface
+     */
+    final public function getCipher() {}
+
+    /**
+     * Returns the message authentication code algorithm used
+     * @return \Cryptopp\MacInterface
+     */
+    final public function getMac() {}
+}
