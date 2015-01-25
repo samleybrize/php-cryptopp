f93d7b3ca5e550f36005cd3295973578bb9835f6
Stephen Berquet <stephen.berquet@gmail.com>
2015-01-25 02:26:54 +0100

    attempt to fix php 5.3 build

--- src/utils/zend_object_utils.h
+++ src/utils/zend_object_utils.h
@@ -34,15 +34,18 @@ zend_object_value zend_custom_create_handler(zend_class_entry *type TSRMLS_DC) {
     memset(obj, 0, sizeof(CONTAINER));
 
     zend_object_std_init(&obj->std, type TSRMLS_CC);
+    object_properties_init(&obj->std, type);
+//    ALLOC_HASHTABLE(obj->std.properties);
+//    zend_hash_init(obj->std.properties, 0, NULL, ZVAL_PTR_DTOR, 0);
 
-    #if PHP_VERSION_ID < 50399
-        zval *tmp;
-        zend_hash_copy(obj->std.properties, &type->properties_info, (copy_ctor_func_t)zval_add_ref, (void *)&tmp, sizeof(zval *));
-    #else
-        object_properties_init(static_cast<zend_object*>(&(obj->std)), type);
-    #endif
+//    #if PHP_VERSION_ID < 50399
+//        zval *tmp;
+//        zend_hash_copy(obj->std.properties, &type->properties_info, (copy_ctor_func_t)zval_add_ref, (void *)&tmp, sizeof(zval *));
+//    #else
+//        object_properties_init(static_cast<zend_object*>(&(obj->std)), type);
+//    #endif
 
-    retval.handle   = zend_objects_store_put(obj, NULL, FREE_STORAGE, NULL TSRMLS_CC);
+    retval.handle   = zend_objects_store_put(obj, (zend_objects_store_dtor_t) zend_objects_destroy_object, FREE_STORAGE, NULL TSRMLS_CC);
     retval.handlers = OBJECT_HANDLERS;
 
     return retval;
